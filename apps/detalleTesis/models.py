4ac44979042968f0807b237d416f7521fee5ad48
zuri <matiaszurita931@gmail.com>
2023-12-04 01:23:30 -0300

    Parche 01 para modelo del kairoz

--- apps/detalleTesis/models.py
+++ apps/detalleTesis/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 from apps.usuarios.models import Usuarios
 from apps.tesis.models import Tesis
-from apps.tesis.models import Tesis
+
 # Create your models here.
 
 class DetalleTesis(models.Model):
@@ -13,7 +13,7 @@ class DetalleTesis(models.Model):
         ]
     id_Usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
     id_tesis = models.ForeignKey(Tesis, on_delete=models.CASCADE)
-    titulo_tesis_relacionado = 
+    titulo_tesis= models.CharField(max_length=255)
     nombre_autor = models.CharField(max_length=255)
     #Visualizacion de profesora para evaluar y marcar aprovado o rechazado 
     mensaje = models.TextField(blank=True)
